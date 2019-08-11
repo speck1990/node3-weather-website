df0f15b9d52fb2ae936833bc18a825ad187e734d
Stephen Peck <speck1990@gmail.com>
2019-08-10 19:43:08 -0500

    Add high and low temp information

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,18 @@ const forecast = (latitude, longitude, callback) => {
 		} else if (body.error) {
 			callback("Unable to find location", undefined);
 		} else {
-			callback(undefined, body.daily.data[0].summary + " It is currently " + body.currently.temperature + " degrees out. There is a " + body.currently.precipProbability + "% chance of rain.");
+			const text =
+				body.daily.data[0].summary +
+				" It is currently " +
+				body.currently.temperature +
+				" degrees out. There is a " +
+				body.currently.precipProbability +
+				"% chance of rain. Today's low is " +
+				body.daily.data[0].temperatureLow +
+				" degrees and the high is " +
+				body.daily.data[0].temperatureHigh +
+				" degrees.";
+			callback(undefined, text);
 		}
 	});
 };
