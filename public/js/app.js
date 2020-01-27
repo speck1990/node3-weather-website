f8a4be6ff129fb9c555d9f0d6a5fb1b0ae2764e4
Stephen Peck <speck1990@gmail.com>
2020-01-26 22:43:02 -0600

    Add current location code and update form submit code

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,34 @@
 const weatherForm = document.querySelector("form");
-const search = document.querySelector("input");
+const search = document.querySelector("input#location");
+const currentLocation = document.querySelector("button#button-current");
 const messageOne = document.querySelector("#message-1");
-const messageTwo = document.querySelector("#message-2");
+
+const currentIcon = document.querySelector("#current-icon");
+const currentTemperature = document.querySelector("#current-temperature");
+const currentState = document.querySelector("#current-state");
+const apparentTemperature = document.querySelector("#apparent-temperature");
+const lowTemperature = document.querySelector("#low-temperature");
+const highTemperature = document.querySelector("#high-temperature");
 
 messageOne.textContent = "";
+
+currentLocation.addEventListener("click", e => {
+	if (!navigator.geolocation) {
+		// no geolocation
+	} else {
+		navigator.geolocation.getCurrentPosition(
+			position => {
+				const latitude = position.coords.latitude;
+				const longitude = position.coords.longitude;
+
+				search.value = latitude + ", " + longitude;
+			},
+			error => {
+				console.log(error);
+			}
+		);
+	}
+});
 
 weatherForm.addEventListener("submit", e => {
 	e.preventDefault();
@@ -11,15 +36,32 @@ weatherForm.addEventListener("submit", e => {
 	const location = encodeURIComponent(search.value);
 
 	messageOne.textContent = "Loading...";
-	messageTwo.textContent = "";
 
 	fetch("/weather?address=" + location).then(response => {
 		response.json().then(data => {
 			if (data.error) {
 				messageOne.textContent = data.error;
 			} else {
-				messageOne.textContent = data.location;
-				messageTwo.textContent = data.forecast;
+				const weatherIcons = {
+					"clear-day": '<i class="fas fa-sun"></i>',
+					"clear-night": '<i class="fas fa-moon"></i>',
+					rain: '<i class="fas fa-cloud-rain"></i>',
+					snow: '<i class="fas fa-snowflake"></i>',
+					sleet: '<i class="fas fa-cloud-meatball"></i>',
+					wind: '<i class="fas fa-wind"></i>',
+					fog: '<i class="fas fa-smog"></i>',
+					cloudy: '<i class="fas fa-cloud"></i>',
+					"partly-cloudy-day": '<i class="fas fa-cloud-sun"></i>',
+					"partly-cloudy-night": '<i class="fas fa-cloud-moon"></i>'
+				};
+
+				currentIcon.innerHTML = weatherIcons[data.forecast.currently.icon];
+				currentTemperature.textContent = Math.round(data.forecast.currently.temperature);
+				currentState.textContent = data.forecast.currently.summary;
+				apparentTemperature.textContent = Math.round(data.forecast.currently.apparentTemperature);
+				lowTemperature.textContent = Math.floor(data.forecast.daily.data[0].temperatureLow);
+				highTemperature.textContent = Math.round(data.forecast.daily.data[0].temperatureHigh);
+				messageOne.textContent = "";
 			}
 		});
 	});
