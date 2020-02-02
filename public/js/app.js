17c7d0ac52ebf8109d1e5b80b58a5f9b65b9ca8b
Stephen Peck <speck1990@gmail.com>
2020-02-02 16:16:58 -0600

    Add hourly and daily forecast  - Create hourly and daily forecast functions  - Create listeners for the hourly and daily forecast functions  - Change "Weekly" button to "Daily" button  - Add forecast table for data to be displayed  - Link moment.js

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,9 @@ const search = document.querySelector("input#location");
 const currentLocation = document.querySelector("button#button-current");
 const messageOne = document.querySelector("#message-1");
 const weatherDetails = document.querySelector("#weather-details");
+const forecastTable = document.querySelector("#forecastTable");
+const hourlyBtn = document.querySelector("#hourlyBtn");
+const dailyBtn = document.querySelector("#dailyBtn");
 
 const currentIcon = document.querySelector("#current-icon");
 const currentTemperature = document.querySelector("#current-temperature");
@@ -43,18 +46,15 @@ weatherForm.addEventListener("submit", e => {
 			if (data.error) {
 				messageOne.textContent = data.error;
 			} else {
-				const weatherIcons = {
-					"clear-day": '<i class="fas fa-sun"></i>',
-					"clear-night": '<i class="fas fa-moon"></i>',
-					rain: '<i class="fas fa-cloud-rain"></i>',
-					snow: '<i class="fas fa-snowflake"></i>',
-					sleet: '<i class="fas fa-cloud-meatball"></i>',
-					wind: '<i class="fas fa-wind"></i>',
-					fog: '<i class="fas fa-smog"></i>',
-					cloudy: '<i class="fas fa-cloud"></i>',
-					"partly-cloudy-day": '<i class="fas fa-cloud-sun"></i>',
-					"partly-cloudy-night": '<i class="fas fa-cloud-moon"></i>'
-				};
+				forecastTable.innerHTML = hourlyForecast(data.forecast.hourly.data, 12);
+
+				hourlyBtn.addEventListener("click", e => {
+					forecastTable.innerHTML = hourlyForecast(data.forecast.hourly.data, 7);
+				});
+
+				dailyBtn.addEventListener("click", e => {
+					forecastTable.innerHTML = dailyForecast(data.forecast.daily.data, 7);
+				});
 
 				currentIcon.innerHTML = weatherIcons[data.forecast.currently.icon];
 				currentTemperature.textContent = Math.round(data.forecast.currently.temperature);
@@ -69,3 +69,70 @@ weatherForm.addEventListener("submit", e => {
 		});
 	});
 });
+
+const weatherIcons = {
+	"clear-day": '<i class="fas fa-sun"></i>',
+	"clear-night": '<i class="fas fa-moon"></i>',
+	rain: '<i class="fas fa-cloud-rain"></i>',
+	snow: '<i class="fas fa-snowflake"></i>',
+	sleet: '<i class="fas fa-cloud-meatball"></i>',
+	wind: '<i class="fas fa-wind"></i>',
+	fog: '<i class="fas fa-smog"></i>',
+	cloudy: '<i class="fas fa-cloud"></i>',
+	"partly-cloudy-day": '<i class="fas fa-cloud-sun"></i>',
+	"partly-cloudy-night": '<i class="fas fa-cloud-moon"></i>'
+};
+
+const hourlyForecast = (data, limit) => {
+	let forecast = `<thead>
+						<tr>
+							<th scope="col">Time</th>
+							<th scope="col"></th>
+							<th scope="col">Description</th>
+							<th scope="col">Temperature</th>
+							<th scope="col">Precip</th>
+						</tr>
+					</thead>
+					<tbody>`;
+	for (let i = 0; i <= limit; i++) {
+		const info = data[i];
+		let time = moment(info.time * 1000).format("h A");
+
+		forecast += "<tr>";
+		forecast += `<td>${time}</td>
+					<th scope="row">${weatherIcons[info.icon]}</td>
+					<td>${info.summary}</td>
+					<td>${Math.round(info.temperature)}&deg;</td>
+					<td>${Math.round(info.precipProbability * 10)}%</td>`;
+		forecast += "</tr>";
+	}
+	forecast += "</tbody>";
+	return forecast;
+};
+
+const dailyForecast = (data, limit) => {
+	let forecast = `<thead>
+						<tr>
+							<th scope="col">Day</th>
+							<th scope="col"></th>
+							<th scope="col">Description</th>
+							<th scope="col">High/Low</th>
+							<th scope="col">Precip</th>
+						</tr>
+					</thead>
+					<tbody>`;
+	for (let i = 0; i <= limit; i++) {
+		const info = data[i];
+		let time = moment(info.time * 1000).format("ddd");
+
+		forecast += "<tr>";
+		forecast += `<td>${time}</td>
+					<th scope="row">${weatherIcons[info.icon]}</td>
+					<td>${info.summary}</td>
+					<td>${Math.round(info.temperatureHigh)}&deg; / ${Math.round(info.temperatureLow)}&deg;</td>
+					<td>${Math.round(info.precipProbability * 10)}%</td>`;
+		forecast += "</tr>";
+	}
+	forecast += "</tbody>";
+	return forecast;
+};
